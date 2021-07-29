5da4a5c81c0c50558e328d7abc99d0749de54f5d
jioms0 <88138805+jioms0@users.noreply.github.com>
2021-07-29 15:23:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const express =require('express');
+const app =  new express();
+const axios = require('axios');
+const bodyparser = require('body-parser');
+const request = require('request');
+const { response } = require('express');
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+var home_data=[];
+var options = {
+    headers:
+    {
+       
+        'Fk-Affiliate-Token': '37aab18a3e8e48da95f50ee7e1a6d951',
+        'Fk-Affiliate-Id': 'prasanta13',
+        'Access-Control-Allow-Origin':'*',
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+    }
+};
+
+
+
+
+app.get("/", function(req, res){
+   
+  
+    const myData = function(hData) {
+        home_data= hData;
+         console.log(home_data);
+         return home_data;
+       }
+    axios.get("https://affiliate-api.flipkart.net/affiliate/offers/v1/all/json",options)
+    .then(function(response) { 
+        const hData = response.data.allOffersList;
+        return hData;
+        
+    })
+    
+    .catch(function(error){
+        console.log(error)
+    })
+    .then(myData);
+   
+
+
+
+
+
+    res.render("home", {home_Sldr : home_data});
+
+});
+
+
+
+
+
+
+
+app.listen("3000", function(){console.log("server running on port 3000")});
