6e9f18261eab0aa06733b8fa0eb098d965bb7404
jioms0 <88138805+jioms0@users.noreply.github.com>
2021-07-31 18:01:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ const request = require('request');
 const { response } = require('express');
 app.use(bodyparser.urlencoded({extended:true}));
 var port = process.env.PORT || 8080;
-app.use(express.static('public'));
+app.use(express.static(__dirname + "/public"));
 app.set('view engine', 'ejs');
-
+var koki=[];
 var home_data=[];
+var menu_data=[];
+var prdt_data=[];
 var options = {
     headers:
     {
        
-        'Fk-Affiliate-Token': 'bd6298b80c164655bf12a15d405cf09f',
-        'Fk-Affiliate-Id': 'onlinesho41',
+        'Fk-Affiliate-Token': '37aab18a3e8e48da95f50ee7e1a6d951',
+        'Fk-Affiliate-Id': 'prasanta13',
         'Access-Control-Allow-Origin':'*',
         "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
     }
@@ -25,31 +27,91 @@ var options = {
 
 
 app.get("/", function(req, res){
-   
-  
-    const myData = function(hData) {
-        home_data= hData;
-         console.log(home_data);
-         return home_data;
-       }
-    axios.get("https://affiliate-api.flipkart.net/affiliate/offers/v1/all/json",options)
-    .then(function(response) { 
-        const hData = response.data.allOffersList;
-        return hData;
-        
-    })
+
+       (async() => {
+        const asyncExample = async() => {
+          let data1, data2;
+          try {
+            data1 = await axios.get("https://affiliate-api.flipkart.net/affiliate/api/onlinesho41.json",options);
+            data2 = await axios.get("https://affiliate-api.flipkart.net/affiliate/offers/v1/all/json",options);
+        } catch (err) {
+            console.log(err);
+          }
+          return [data1, data2];
+        };
+      
+        //Save response on a variable
+       const globalData = await asyncExample();
+      
+       menu_data= globalData[0].data.apiGroups.affiliate.apiListings;
+       home_data= globalData[1].data.allOffersList;
+
     
-    .catch(function(error){
-        console.log(error)
-    })
-    .then(myData);
+     
+
+
+
+
+
+
+
+       res.render("home", {home_Sldr : home_data, menu_dat: menu_data, testingg:"vanakkam"} );
+      
+      })();
+
+      
+     
+
+
+
+
+
+
+
+
+
+
+
+
    
 
 
+});
 
 
 
-    res.render("home", {home_Sldr : home_data});
+app.get(/.*category*/, function(req, res){
+
+    var urlpg= req.originalUrl.replace(/%3F/g, "?");
+    urlpg = "https://affiliate-api.flipkart.net"+ urlpg;
+    console.log("-------------------------------ok ---------------------"+ urlpg); 
+    
+    
+
+
+
+    (async() => {
+        const asyncExample = async() => {
+          let data1, data2;
+          try {
+            data1 = await axios.get("https://affiliate-api.flipkart.net/affiliate/api/onlinesho41.json",options);
+            data2 = await axios.get(urlpg,options);
+        } catch (err) {
+            console.log(err);
+          }
+          return [data1, data2];
+        };
+      
+        //Save response on a variable
+       const globalData = await asyncExample();
+
+       menu_data= globalData[0].data.apiGroups.affiliate.apiListings;
+       prdt_data= globalData[1].data.products;
+
+
+  
+   res.render("product", {menu_dat: menu_data, pr_data: prdt_data})       
+})();
 
 });
 
@@ -57,6 +119,4 @@ app.get("/", function(req, res){
 
 
 
-
-
-app.listen(port, function(){console.log("server running on port 3000")});
+app.listen(port, function(){console.log("server running on port 8080")});
